48e18d6f6daf37da241ab0445b81f1b7fa8c1be9
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-27 21:07:03 -0600

    sortBy - Pipe personalizado

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -10,7 +10,7 @@ export class ToggleCasePipe implements PipeTransform {
 
   transform( value: string, toUpper: boolean = false ): any {
 
-    console.log({ value, toUpper });
+    // console.log({ value, toUpper });
     return ( toUpper )
     ? value.toUpperCase()
     : value.toLowerCase();
