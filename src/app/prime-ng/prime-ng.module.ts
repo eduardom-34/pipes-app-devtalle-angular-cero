b16043ea4704da1bd32f5df2811333706499d1ca
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-27 20:18:47 -0600

    PrimeNG - Table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 // import { MenuModule } from 'primeng/menu';
 // import { InputTextModule } from 'primeng/inputtext';
 // import { RippleModule } from 'primeng/ripple';
+
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
 import { ToolbarModule } from 'primeng/toolbar';
 
 
@@ -22,7 +24,8 @@ import { ToolbarModule } from 'primeng/toolbar';
     FieldsetModule,
     MenubarModule,
     PanelModule,
-    ToolbarModule
+    TableModule,
+    ToolbarModule,
 
   ]
 })
--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -8,6 +9,35 @@ import { Component } from '@angular/core';
 export class OrderComponent {
 
   public isUpperCase: boolean = false;
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Bataman',
+      canFly: false,
+      color: Color.black
+    },
+    {
+      name: 'Daredevil',
+      canFly: false,
+      color: Color.red
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.red
+    },
+    {
+      name: 'Linterna verda',
+      canFly: true,
+      color: Color.green
+    },
+
+  ]
 
   toggleUpperCase():void {
     this.isUpperCase = !this.isUpperCase;
